a4db88cde94ed262713c749ca0f90703957e4379
Tyler Alden Gubala <gubalatyler@yahoo.com>
2018-01-14 17:24:40 -0500

    added make script

---
+++ make.bat
@@ -0,0 +1,88 @@
+@echo off
+
+if defined PROCESSOR_ARCHITECTURE (
+    if /I "%PROCESSOR_ARCHITECTURE%"=="amd64" (
+        set platform="64"
+        call :setup
+        call :build
+        call :install
+        echo Successfully built Lua for windows %platform%
+        goto :eof
+    )
+    if /I "%PROCESSOR_ARCHITECTURE%"=="x86" (
+        set platform="32"
+        call :setup
+        call :build
+        call :install
+        echo Successfully built Lua for windows %platform%
+        goto :eof
+    )
+)
+if defined PROCESSOR_ARCHITECTUREW6432 (
+    if /I "%PROCESSOR_ARCHITECTUREW6432%"=="amd64" (
+        set platform="64"
+        call :setup
+        call :build
+        call :install
+        echo Successfully built Lua for windows %platform%
+        goto :eof
+    )
+    if /I "%PROCESSOR_ARCHITECTUREW6432%"=="x86" (
+        set platform="32"
+        call :setup
+        call :build
+        call :install
+        echo Successfully built Lua for windows %platform%
+        goto :eof
+    )
+)
+call :archinvalid
+goto :eof
+
+:archinvalid
+echo "Your platform archetecture could not be determined: " platform
+goto :eof
+
+:argerror
+echo "Platform must be one of: %platforms%"
+goto :eof
+
+:vserror
+echo Visual Studio 2017 not installed, or the environment variable VS2017INSTALLDIR is not set, please remedy this and try again
+goto :eof
+
+:setup
+if defined VS2017INSTALLDIR (
+    if defined platform (echo "Platform is defined") else (echo "Platform is not defined")
+    call "%VS2017INSTALLDIR%\VC\Auxiliary\Build\vcvars%platform%"
+    goto :eof
+)
+else (
+    call :vserror
+    goto :eof
+)
+
+:build
+cl /MD /O2 /c /DLUA_BUILD_AS_DLL *.c
+ren lua.obj lua.o
+ren luac.obj luac.o
+link /DLL /IMPLIB:lua.lib /OUT:lua.dll *.obj 
+link /OUT:lua.exe lua.o lua.lib 
+lib /OUT:lua-static.lib *.obj
+link /OUT:luac.exe luac.o lua-static.lib
+goto :eof
+
+:install
+if not exist "%LOCALAPPDATA%\Lua" mkdir "%LOCALAPPDATA%\Lua"
+if if not exist "%LOCALAPPDATA%\Lua\include" mkdir "%LOCALAPPDATA%\Lua\include"
+if not exist "%LOCALAPPDATA%\Lua\bin" mkdir "%LOCALAPPDATA%\Lua\bin"
+if not exist "%LOCALAPPDATA%\Lua\src" mkdir "%LOCALAPPDATA%\Lua\src"
+xcopy /y *.exe "%LOCALAPPDATA%\Lua"
+xcopy /y *.dll "%LOCALAPPDATA%\Lua\bin"
+xcopy /y *.h "%LOCALAPPDATA%\Lua\include"
+xcopy /y *.s "%LOCALAPPDATA%\Lua\src"
+echo ""
+echo Your DLL and exe files are in %LOCALAPPDATA%\Lua
+echo Setting path
+setx PATH "%PATH%;%LOCALAPPDATA%\Lua"
+goto :eof
